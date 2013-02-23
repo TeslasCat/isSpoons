5ebdd89e445ab2821247e7a3fda884c3c12dec2a
flabbyrabbit <flabbyrabbit@gmail.com>
2013-02-23 17:01:21 +0000

    Added jQuery as well as moving javascript to its own file

--- html/index.php
+++ html/index.php
@@ -10,34 +10,13 @@
 		<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.ie.css" />
 		<![endif]-->
 		<link rel="stylesheet" href="css/main.css" />
-
-		<script src="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
-		<script src="js/leaflet.markercluster.js"></script>
 	</head>
 	<body>
 		<div id="map" style="width: 100%; height:100%"></div>
-		<script>var map = L.map('map').setView([51.505, -0.09], 13);
 
-			// add an OpenStreetMap tile layer
-			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-			}).addTo(map);
-
-			var markers = new L.MarkerClusterGroup({ maxClusterRadius: 100});
-			map.addLayer(markers);
-			// add a marker in the given location, attach some popup content to it and open the popup
-<?php
-	$file = "/home/flabbyrabbit/git/isSpoons/files/locations.json";
-	$data = file_get_contents($file);
-	$pubs = json_decode($data);
-
-	foreach ($pubs as $pub) {
-		if ($pub->location != null)
-			echo "var m = new L.marker([".$pub->location->lat.", ".$pub->location->lng."]).bindPopup('".str_replace("'", "", $pub->name)."');	markers.addLayer(m);\n";
-	}
-?>
-			
-		</script>
-
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+		<script src="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
+		<script src="js/leaflet.markercluster.js"></script>
+		<script src="js/main.js"></script>
 	</body>
 </html>
