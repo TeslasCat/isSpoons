8905bda6c01c938f6a1e09a59dfc58ffbef09500
flabbyrabbit <flabbyrabbit@gmail.com>
2013-02-24 04:21:10 +0000

    Changed marker popups to information overlay

--- html/index.php
+++ html/index.php
@@ -5,6 +5,7 @@
 		<title>isSpoons</title>
 		<meta name="description" content="The HTML5 Herald">
 		<meta name="author" content="SitePoint">
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.css" />
 		<!--[if lte IE 8]>
 		<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.ie.css" />
@@ -13,6 +14,10 @@
 	</head>
 	<body>
 		<div id="map" style="width: 100%; height:100%"></div>
+
+		<div class='overlay' id='marker-information'>
+			<h1>Title</h1>
+		</div>
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
 		<script src="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
--- html/js/main.js
+++ html/js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+	var pubs;
 	var map = L.map('map').setView([54.8, -4], 6);	
 	// add an OpenStreetMap tile layer
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -9,15 +10,24 @@ $(function() {
 	map.addLayer(markers);
 
 	$.getJSON("files/locations.json", function(data) {
-			$.each(data, function(key, pub) {
-				if (pub.location != null) {
-					var m = new L.marker([pub.location.lat, pub.location.lng]).bindPopup(pub.name);
-					markers.addLayer(m);
-				}
-			})
+		pubs = data;
+		i = 0;
+		$.each(pubs, function(key, pub) {
+			if (pub.location != null) {
+				var m = new L.marker([pub.location.lat, pub.location.lng]);
+				m.on('click', (function(i) { return function(e) { show_popup(i) } }(i)));
+
+				markers.addLayer(m);
+			}
+			i++;
+		});
 	});
 
-	map.on('moveend', function(e) {
-		console.log(e);
-	});
+	function show_popup(marker) {
+		var pub = pubs[marker];
+
+
+		$('#marker-information h1').text(pub.name);
+		$('#marker-information').show();
+	}
 });
